8a5a1f16a237641ff7070133fa661baf9a672783
Shystra <jlsp.2019@hotmail.com>
2023-09-06 23:25:43 -0300

    test

--- project/src/components/EndForm/EndForm.tsx
+++ project/src/components/EndForm/EndForm.tsx
@@ -1,12 +1,32 @@
+import styles from './EndForm.module.css';
 
+[]
 type Props = {
     onNext: (value: string) => void;
     onBack: () => void;
 }
 export const EndForm = ({ onNext, onBack }: Props) => {
     return (
-        <div>
-            testing
+        <div className={styles.container_end}>
+            {currentStep === 0 && (
+            <>
+                <label>Nome</label>
+                <input type="text" />
+
+                <label>CEP do Imóvel</label>
+                <input type="text" />
+
+
+                <label>E-mail</label>
+                <input type="text" />
+
+                <label>Telefone</label>
+                <input type="text" />
+            
+          </>
+          )}
+          
         </div>
+        
     )
 }
