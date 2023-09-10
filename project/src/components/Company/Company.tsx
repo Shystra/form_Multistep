4a69fcc480c11358758f69036b2003c77848562a
Shystra <jlsp.2019@hotmail.com>
2023-09-09 22:14:27 -0300

    new page for company

--- project/src/components/Company/Company.tsx
+++ project/src/components/Company/Company.tsx
@@ -1,15 +1,46 @@
-
+import styles from './Company.module.css';
 
 type Props = {
     onNext: (value: string) => void;
     onBack: () => void;
 }
 export const Company = ({}: Props) => {
+    
+
+
     return (
-        <div>
-            <h1>Company</h1>
-            <h1>Company</h1>
-            <h1>Company</h1>
+        <div className={styles.container_option_company}>
+            <>
+            <div className={styles.progressOne_option_company}></div>
+                <label className={styles.progressLabel_option_company}></label>
+                    <h1 className={styles.title_option_company}>
+                        Selecione uma das opções:</h1>
+
+            <div className={styles.buttonsWrapper_option_company}>
+                <div className={styles.option_street_commerce}>
+                    <button>Comércio de Rua</button>
+                </div>
+
+                <div className={styles.option_company_shopping_center}>
+                    <button>Empresa em centro comercial</button>
+                </div>
+
+                <div className={styles.option_company_industrial_area}>
+                    <button>Empresa em área industrial</button>
+                </div>
+
+                <div className={styles.optio_business_building}>
+                    <button>Prédio comercial</button>
+                </div>
+            </div>
+
+            <div className={styles.button_back}>
+                <button>Voltar</button>
+            </div>
+            
+            </>
+
+
         </div>
     )
 };
